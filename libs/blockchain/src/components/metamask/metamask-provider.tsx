e61db4ba7e85105604d63bfd7d04d0b0c2032682
lucafan8973 <ferexmoto6@gmail.com>
2026-01-21 20:56:57 +0900

    commit

--- libs/blockchain/src/components/metamask/metamask-provider.tsx
+++ libs/blockchain/src/components/metamask/metamask-provider.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MetaMaskProvider as Provider } from '@metamask/sdk-react';
 import { APP_NAME, INFURA_API_KEY } from '../../utils/constants';
 
 export const MetaMaskProvider = ({
@@ -8,19 +7,7 @@ export const MetaMaskProvider = ({
   children: React.ReactNode;
 }) => {
   return (
-    <Provider
-      debug={false}
-      sdkOptions={{
-        dappMetadata: {
-          name: APP_NAME,
-          url: window.location.href,
-        },
-        infuraAPIKey: INFURA_API_KEY,
-        // Other options
-      }}
-    >
-      {children}
-    </Provider>
+    <></>
   );
 };
 
