fd1de3a374ee84b0354f661aa2c0c5a37e3c984b
lucafan8973 <ferexmoto6@gmail.com>
2026-01-21 21:00:44 +0900

    commit

--- apps/dapp/src/main.tsx
+++ apps/dapp/src/main.tsx
@@ -2,15 +2,12 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app';
 import { BrowserRouter } from 'react-router-dom';
-import { MetaMaskProvider } from '@defi-token/blockchain';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <MetaMaskProvider>
       <App />
-    </MetaMaskProvider>
   </BrowserRouter>
 );
